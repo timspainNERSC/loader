9d2834847ed1de2227e88fc331e9babf9331d9a6
Tim Spain <timothy.spain@nersc.no>
2021-09-27 16:14:38 +0200

    Pylint the python file.

--- src/moduleloader_builder.py
+++ src/moduleloader_builder.py
@@ -1,31 +1,38 @@
-def iName(name):
+"""This module generates the inclusion .ipp files for the C++ ModuleLoader."""
+
+def get_iname(name):
+    """Returns the name of the interface class, given its string name."""
     return "I" + name
-            
-def pName(i_name):
+
+def get_pname(i_name):
+    """Returns the name of the function pointer for the interface, given its class name."""
     return "p_" + i_name
 
-def fName(impl):
+def get_fname(impl):
+    """Returns the function name for the implementation, given its string name."""
     return "new" + impl
 
 def headers(all_implementations, ipp_prefix, hpp_prefix):
-    with open(ipp_prefix + "moduleLoaderHeaders.ipp", "w") as f:
+    """Generates the moduleLoaderHeaders.ipp file."""
+    with open(ipp_prefix + "moduleLoaderHeaders.ipp", "w", encoding="utf-8") as file:
         for interface in all_implementations:
-            i_name = iName(interface["name"])
-            f.write("#include \"" + hpp_prefix + i_name + ".hpp\"\n")
+            i_name = get_iname(interface["name"])
+            file.write("#include \"" + hpp_prefix + i_name + ".hpp\"\n")
             for impl in interface["implementations"]:
-                f.write("#include \"" +hpp_prefix + impl + ".hpp\"\n")
+                file.write("#include \"" +hpp_prefix + impl + ".hpp\"\n")
             # An extra line between interfaces
-            f.write("\n")
+            file.write("\n")
 
 def functions(all_implementations, ipp_prefix):
-    with open(ipp_prefix + "moduleLoaderFunctions.ipp", "w") as f:
+    """Generates the moduleLoaderFunctions.ipp file."""
+    with open(ipp_prefix + "moduleLoaderFunctions.ipp", "w", encoding="utf-8") as file:
         for interface in all_implementations:
             # Define the pointer to function
-            i_name = iName(interface["name"])
-            p_name = pName(i_name)
-            f.write("std::unique_ptr<" + i_name + "> (*" + p_name + ")();\n")
+            i_name = get_iname(interface["name"])
+            p_name = get_pname(i_name)
+            file.write("std::unique_ptr<" + i_name + "> (*" + p_name + ")();\n")
             # Define function that call the function pointer
-            f.write(
+            file.write(
                 "template<>\n" +
                 "std::unique_ptr<" + i_name + "> ModuleLoader::getImplementation() const\n" +
                 "{\n" +
@@ -33,68 +40,69 @@ def functions(all_implementations, ipp_prefix):
                 "}\n"
                 )
             for impl in interface["implementations"]:
-                f.write("// " + impl + "\n")
+                file.write("// " + impl + "\n")
                 # The function that return the new unique_ptr for each implementation
-                f.write(
-                    "std::unique_ptr<" + i_name + "> " + fName(impl) + "()\n" +
+                file.write(
+                    "std::unique_ptr<" + i_name + "> " + get_fname(impl) + "()\n" +
                     "{\n" +
                     "    return std::unique_ptr<" + impl + ">(new " + impl + ");\n" +
                     "}\n"
                     )
             # An extra line between interfaces
-            f.write("\n")
+            file.write("\n")
 
 def names(all_implementations, ipp_prefix):
-    with open(ipp_prefix + "moduleLoaderNames.ipp", "w") as f:
-        f.write(
+    """Generates the moduleLoaderNames.ipp file."""
+    with open(ipp_prefix + "moduleLoaderNames.ipp", "w", encoding="utf-8") as file:
+        file.write(
             "    m_availableImplementationNames = {\n" +
             "        "
             )
         for interface in all_implementations:
             name = interface["name"]
-            f.write(
+            file.write(
                 "{\n" +
                 "        \"" + name + "\", {\n"
                 )
             for impl in interface["implementations"]:
-                f.write("            \"" + impl + "\",\n")
-            f.write(
+                file.write("            \"" + impl + "\",\n")
+            file.write(
                 "            }\n" +
                 "        },"
                 )
-        f.write("\n    };\n")
-        
+        file.write("\n    };\n")
 
 def assignments(all_implementations, ipp_prefix):
-    with open(ipp_prefix + "moduleLoaderAssignments.ipp", "w") as f:
-        f.write("        ")
+    """Generates the moduleLoaderAssignments.ipp file."""
+    with open(ipp_prefix + "moduleLoaderAssignments.ipp", "w", encoding="utf-8") as file:
+        file.write("        ")
         for interface in all_implementations:
             name = interface["name"]
-            i_name = iName(name)
-            f.write(
+            i_name = get_iname(name)
+            file.write(
                 "if (module == \"" + name + "\") {\n" +
                 "            "
                 )
-            p_name = pName(i_name)
+            p_name = get_pname(i_name)
             for impl in interface["implementations"]:
-                f.write(
+                file.write(
                     "if (impl == \"" + impl + "\") {\n"
-                    "                " + p_name + " = &" + fName(impl) + ";\n" +
+                    "                " + p_name + " = &" + get_fname(impl) + ";\n" +
                     "            } else "
                     )
-            f.write(
+            file.write(
                 "{\n" +
                 "                throwup(module, impl);\n" +
                 "            }\n" +
                 "\n" +
                 "        } else "
                 )
-        f.write("{ }")
+        file.write("{ }")
 
 def generate(all_implementations, ipp_prefix = '', hpp_prefix = ''):
     """Generates the .ipp inclusion files for ModuleLoader.cpp
 
-    :param all_implementations: The vector of dictionaries that defines the 
+    :param all_implementations: The vector of dictionaries that defines the
            interfaces and implementations thereof.
     :param prefix: A directory and file prefix that preceeds the file names to
            provide a path from the current working directory.
@@ -114,5 +122,5 @@ if __name__ == "__main__":
     ithe = {"name": "thermodynamics", "implementations": ["thermoWinton", "thermoIce0"]}
 
     alli = [iAlb, ithe]
-    
+
     generate(alli, hpp_prefix = "include/")
